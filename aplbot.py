56f74d2cefcc9a5eb8f7621bb742216997a1d9bd
hyper-neutrino <alex.yj.liao@gmail.com>
2021-12-27 16:29:57 -0500

    fix indentation

--- aplbot.py
+++ aplbot.py
@@ -37,13 +37,13 @@ def response_for(x):
         if line.startswith("⋄"):
           codes.append(preparse(line[1:]))
   else:
-      if x["content"].startswith("⎕&larr;") or x["content"].startswith("⋄"):
-          item = "⋄ " if x["content"].startswith("⋄") else "⎕←"
-          return rf":{x['message_id']} Did you forget to add backticks around your code (`\`{item}code\``)? You can edit your message and I will edit my reply."
-      if "`⎕&larr" in x["content"]:
-          return rf":{x['message_id']} Did you forget a closing backtick (`\`⎕←code\``)? You can edit your message and I will edit my reply."
-      if "`⋄" in x["content"]:
-          return rf":{x['message_id']} Did you forget a closing backtick (`\`⋄code\``)? You can edit your message and I will edit my reply."
+    if x["content"].startswith("⎕&larr;") or x["content"].startswith("⋄"):
+      item = "⋄ " if x["content"].startswith("⋄") else "⎕←"
+      return rf":{x['message_id']} Did you forget to add backticks around your code (`\`{item}code\``)? You can edit your message and I will edit my reply."
+    if "`⎕&larr" in x["content"]:
+      return rf":{x['message_id']} Did you forget a closing backtick (`\`⎕←code\``)? You can edit your message and I will edit my reply."
+    if "`⋄" in x["content"]:
+      return rf":{x['message_id']} Did you forget a closing backtick (`\`⋄code\``)? You can edit your message and I will edit my reply."
     for block in re.findall(r"<code>((⎕&larr;|⋄).*?)</code>", x["content"]):
       if block[0] == "⎕&larr;" or block[0] == "⋄": continue
       codes.append(preparse(html.unescape(block[0])))
